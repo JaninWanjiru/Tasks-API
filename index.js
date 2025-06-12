3a0426c0bfafb02b5277f2846f26c82142516d35
JaninWanjiru <janinndongo02@gmail.com>
2025-06-12 18:52:45 +0300

    add a request to get all tasks

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ import { PrismaClient } from '@prisma/client';
 const app = express()
 const client = new PrismaClient()
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.send("<h1>You've unlocked the Tasks API</h1>")
 })
+
+app.get("/AllTasks", async (_req, res) => {
+    try {
+        const tasks = await client.task.findMany();
+        res.status(200).json(tasks)
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ message: "There's an issue somewhere"})
+    }
+}); 
 
 let port = process.env.PORT || 8800;
 
