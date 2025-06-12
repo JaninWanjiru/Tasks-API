7c1c68c98cc679f1f162b129926cdde63d1a3738
JaninWanjiru <janinndongo02@gmail.com>
2025-06-12 19:22:29 +0300

    add a request to create a task

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import { PrismaClient } from '@prisma/client';
 const app = express()
 const client = new PrismaClient()
 
+app.use(express.json());
+
 app.get("/", (_req, res) => {
     res.send("<h1>You've unlocked the Tasks API</h1>")
 })
 
-app.get("/AllTasks", async (_req, res) => {
+app.get("/tasks", async (_req, res) => {
     try {
         const tasks = await client.task.findMany();
         res.status(200).json(tasks)
@@ -17,6 +19,21 @@ app.get("/AllTasks", async (_req, res) => {
         res.status(500).json({ message: "There's an issue somewhere"})
     }
 }); 
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const {title, description} = req.body
+        const newTask = await client.task.create({
+            data: {
+                title,
+                description
+            }
+        })
+        res.status(201).json(newTask)
+    } catch (e) {
+        res.status(500).json({message: "There's an issue somewhere"})
+    }
+});
 
 let port = process.env.PORT || 8800;
 
